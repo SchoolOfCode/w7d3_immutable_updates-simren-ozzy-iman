7a5b28d736441179c4a411885a439583d6e3718e
ozzyee <62845796+ozzyee@users.noreply.github.com>
2022-01-06 12:19:50 +0000

    feat: task 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+   const newArray = [...array, item];
+   return newArray;
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+   const newArray = [item, ...array];
+   return newArray;
+}
 
 // Immutably insert an item at a specific position/index within an array
 export function insertItem(array, item, index) {}
